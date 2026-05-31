9d3edae0cd09ab9d7024e4844bd506afdaf36135
khoamobile111977 <141764455+khoamobile111977@users.noreply.github.com>
2026-05-31 19:20:03 +0700

    Update autodarkfragment.lua

--- autodarkfragment.lua
+++ autodarkfragment.lua
@@ -1,13 +1,13 @@
 repeat wait() until game:IsLoaded()
 repeat wait() until game.Players and game.Players.LocalPlayer
-task.wait(3)
+task.wait(5)
 if not game.Players.LocalPlayer.Team then
     game.ReplicatedStorage.Remotes.CommF_:InvokeServer("SetTeam", "Marines")
 end
 
 repeat task.wait()
 until game.Players.LocalPlayer.Team
-task.wait(3)
+task.wait(2)
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
